c8b5aad6bca70e560f1e6f12b645fefa666eb59c
Virus <yobramoses690@gmail.com>
2025-12-18 14:01:16 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,8 +1,8 @@
-//=====💫 sᴛᴀʀ xᴅ🔥====
+//=====💫 virus-XD🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
+  BOT_NAME: 'virus-XD', // ur bot name
+  OWNER_NAME: 'virus-XD', //your name
   OWNER_NUMBER: '263776388689', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
